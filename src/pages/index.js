7f471977ffe00122ed7adfdc16c49b18b1595c79
Min121e <97957887+Min121e@users.noreply.github.com>
2023-12-14 21:15:49 +0630

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Banner from '@/components/Banner.js';
 import Header from '../components/Header.js';
 import Head from "next/head";
 import ProductFeed from '@/components/ProductFeed.js';
-// import { Provider } from 'react-redux';
 import Footer from '@/components/footer/Footer.js';
 
 export default function Home({}) {
@@ -17,8 +16,6 @@ export default function Home({}) {
       <main className='max-w-screen-2xl mx-auto '>
         <Banner />
 
-        {/* <ProductFeed products={products}/> */}
-
         <ProductFeed />
 
         <Footer />
@@ -27,15 +24,3 @@ export default function Home({}) {
     </div>
   );
 }
-
-// export async function getServerSideProps(context) {
-//   const products = await fetch('https://fakestoreapi.com/products')
-//   .then((res) => res.json())
-//   return {
-//     props: {
-//       products,
-//     }
-//   }
-// }
-
-// https://fakestoreapi.com/products
